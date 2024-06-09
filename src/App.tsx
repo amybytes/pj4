347d37d684a17a41c06e6403d416975b27319539
rowak <rowak12@gmail.com>
2024-06-09 17:52:18 -0400

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'src/App.css';
+import Sidebar from 'components/Sidebar';
+import Editor from 'components/Editor';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Editor />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import AppLogo from 'assets/logo.png';
+
+function Sidebar() {
+  return (
+    <div>
+      <div>
+        <AppLogo />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
